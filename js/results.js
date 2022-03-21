12a0e5f3e83bd9634515e5d3ae5c67d69054eda0
Henry Cullen <119402282@umail.ucc.ie>
2022-03-21 00:07:56 +0000

    updates to the {message, data}

--- js/results.js
+++ js/results.js
@@ -9,77 +9,81 @@ let btnControls = document.getElementById('btnControls');
 let allForms;
 const app = (input) => {
     
-    let {message} = input;
+    let {message, data} = input;
+    
     //Write your code based on responses here
-    if(message !== "No posts found!" && message.length > 0) {
-        form.reset();
-        alert(message);
-        form.reset();
-    } else if (message === "No posts found!") {
-        logCont.classList.toggle('d-none');
-        resTable.classList.toggle('d-none');
-        result.innerHTML = '<h2>No submissions yet.</h2>';
-    } else if(input['data'].length > 0) {
-        result.innerHTML = input['data'].map((row, index) =>{
-            return `<tr>
-                        <form id="frm${index}" class="clearOne" method="post" action="./../api/delete.php?${encodeURI({ 
-                            action: 'one',
-                            phone: row.phone,
-                            name: row.full_name 
-                        })}" >${
-                            `<td>${row.phone}</td>`+
-                            `<td>${row.full_name}</td>`+
-                            `<td><button type="submit" value="Submit" form="frm${index}" >Delete entry</button></td>`
-                        }
-                        </form>
-                    </tr>`;
-        }).join('\n');
-        console.table(input['data']);
-        foot.innerHTML = "Total Responses: " + input['data'].length;
-        
-        
-
-        if(btnControls.classList.length !==2){
-            const deleteAll = document.createElement('button');
-            deleteAll.setAttribute("id", "deleteAll");
-            deleteAll.innerText = "Clear All Entries";
-            deleteAll.onclick = () => {
-                deleteAllData();
-            };
-            
-            const excel = document.createElement('button');
-            excel.innerText = "Export to Excel";
-            excel.onclick = () => {
-                $("#resultsTable").table2excel({
-                    name: "survey",
-                    exclude_inputs: true,
-                    filename: "Results.xls", // do include extension
-                    preserveColors: false 
-                });
-            }
-            btnControls.appendChild(deleteAll);
-            btnControls.appendChild(excel);
+    if(message){
+        if(message !== "No posts found!") {
+            form.reset();
+            alert(message);
+            form.reset();
+        } else if (message === "No posts found!") {
+            logCont.classList.toggle('d-none');
+            resTable.classList.toggle('d-none');
+            result.innerHTML = '<h2>No submissions yet.</h2>';
         }
-        
-        allForms = document.querySelectorAll('form.clearOne');
-        for (let i = 0; i < allForms.length; i++){
-            allForms[i].onsubmit = (event) => {
-                event.preventDefault();
-                let datafiedURL =allForms[i].getAttribute('action');
-                fetch(datafiedURL).then((response) => {
-
-                }).finally(() => {
-                    relTable();
-                });
-            }
-        }
-        logCont.classList.toggle('d-none');
-        resTable.classList.toggle('d-none');
-        form.reset();
-        
     }
+    if(data){
+        if(data.length > 0) {
+            result.innerHTML = input['data'].map((row, index) =>{
+                return `<tr>
+                            <form id="frm${index}" class="clearOne" method="post" action="./../api/delete.php?${encodeURI({ 
+                                action: 'one',
+                                phone: row.phone,
+                                name: row.full_name 
+                            })}" >${
+                                `<td>${row.phone}</td>`+
+                                `<td>${row.full_name}</td>`+
+                                `<td><button type="submit" value="Submit" form="frm${index}" >Delete entry</button></td>`
+                            }
+                            </form>
+                        </tr>`;
+            }).join('\n');
+            console.table(input['data']);
+            foot.innerHTML = "Total Responses: " + input['data'].length;
+            
+            
 
+            if(btnControls.classList.length !==2){
+                const deleteAll = document.createElement('button');
+                deleteAll.setAttribute("id", "deleteAll");
+                deleteAll.innerText = "Clear All Entries";
+                deleteAll.onclick = () => {
+                    deleteAllData();
+                };
+                
+                const excel = document.createElement('button');
+                excel.innerText = "Export to Excel";
+                excel.onclick = () => {
+                    $("#resultsTable").table2excel({
+                        name: "survey",
+                        exclude_inputs: true,
+                        filename: "Results.xls", // do include extension
+                        preserveColors: false 
+                    });
+                }
+                btnControls.appendChild(deleteAll);
+                btnControls.appendChild(excel);
+            }
+            
+            allForms = document.querySelectorAll('form.clearOne');
+            for (let i = 0; i < allForms.length; i++){
+                allForms[i].onsubmit = (event) => {
+                    event.preventDefault();
+                    let datafiedURL =allForms[i].getAttribute('action');
+                    fetch(datafiedURL).then((response) => {
 
+                    }).finally(() => {
+                        relTable();
+                    });
+                }
+            }
+            logCont.classList.toggle('d-none');
+            resTable.classList.toggle('d-none');
+            form.reset();
+            
+        }
+    }
 }
 
 const deleteAllData = () => {
